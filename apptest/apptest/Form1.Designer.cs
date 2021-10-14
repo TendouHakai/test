fe77e99b47ed25049fff2754f4c8f42caa784fae
TendouHakai <thanhconghoahai@gmail.com>
2021-10-14 10:04:07 +0700

    test2

--- apptest/apptest/Form1.Designer.cs
+++ apptest/apptest/Form1.Designer.cs
@@ -32,7 +32,9 @@ namespace apptest
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -57,13 +59,24 @@ namespace apptest
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.Size = new System.Drawing.Size(767, 396);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "test2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(314, 50);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "test2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -74,6 +87,7 @@ namespace apptest
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -83,6 +97,7 @@ namespace apptest
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- apptest/apptest/Form1.cs
+++ apptest/apptest/Form1.cs
@@ -16,5 +16,10 @@ namespace apptest
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test2");
+        }
     }
 }
